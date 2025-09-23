3cab08b5de310bdc10318bc2d6301ad85fd4168a
Celso TCE <celso.filgueiras@tce.ap.gov.br>
2025-09-23 10:01:38 -0300

    trocado gift card do ps5 por gift card da amazon

--- src/components/pages/Page10.tsx
+++ src/components/pages/Page10.tsx
@@ -4,7 +4,7 @@ import ScrollIndicator from "../ScrollIndicator";
 export default function Page10() {
   return (
     <div
-      className="page relative bg-gradient-to-br from-blue-600 to-cyan-600 flex flex-col
+      className="page relative bg-gradient-to-br from-yellow-600 to-amber-600 flex flex-col
         font-gift"
     >
       <div className="wrapper flex flex-col">
@@ -16,14 +16,14 @@ export default function Page10() {
             transition={{ duration: 0.75, ease: 'easeInOut' }}
             viewport={{ once: true }}
           >
-            E um gift card do PlayStation 5
+            E um gift card da Amazon
           </motion.span>
         </div>
 
         <motion.div
           className="grow mb-4"
           style={{
-            backgroundImage: 'url(/ps5.png)',
+            backgroundImage: 'url(/amazon.jpg)',
             backgroundSize: 'contain',
             backgroundPosition: 'center center',
             backgroundRepeat: 'no-repeat',
@@ -34,17 +34,15 @@ export default function Page10() {
           viewport={{ once: true }}
         />
 
-        {/* <div className="grow flex flex-col">
-          <motion.img
-            src="/ps5.png"
-            alt=""
-            className="max-w-full max-h-full border-2 border-white rounded"
-            initial={{ opacity: 0, x: 80 }}
-            whileInView={{ opacity: 1, x: 0 }}
-            transition={{ duration: 0.75, delay: 0.80, ease: 'easeInOut' }}
-            viewport={{ once: true }}
-          />
-        </div> */}
+        <motion.div
+          className="text-center text-3xl pb-10"
+          initial={{ opacity: 0, y: -80 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1.0, delay: 1.50, ease: 'easeInOut' }}
+          viewport={{ once: true }}
+        >
+          pra você comprar um livro bem legal!
+        </motion.div>
       </div>
 
       <ScrollIndicator />
